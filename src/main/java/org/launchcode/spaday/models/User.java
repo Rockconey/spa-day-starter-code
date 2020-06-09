d153fe8795fd45a48d554fddf488e65934bd6eff
Shaw Coney <rockconey@gmail.com>
2020-06-08 19:23:01 -0500

    assignment complete

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -10,7 +10,7 @@ public class User {
     public User(String username, String email, String password) {
         this.username = username;
         this.email = email;
-        this.password = password;git
+        this.password = password;
     }
 
     public String getUsername() {
